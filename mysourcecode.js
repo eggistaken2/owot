0ce79d9ca367a01891a007aa922a9da70624ae96
eggistaken2 <131176596+eggistaken2@users.noreply.github.com>
2024-01-03 13:24:21 -0300

    Update mysourcecode.js
    lol

--- mysourcecode.js
+++ mysourcecode.js
@@ -1,1 +1,1 @@
-a=_=>{return {hi:_=>new Notification("hello!"),mySourceCode:"a="+a,codeYouRan:"use('eggistaken2/owot/mysourcecode.js').codeYouRan"}}
+a=_=>{return {hi:_=>new Notification("hello!"),mySourceCode:"a="+a+";a()",codeYouRan:"use('eggistaken2/owot/mysourcecode.js').codeYouRan"}};a()
